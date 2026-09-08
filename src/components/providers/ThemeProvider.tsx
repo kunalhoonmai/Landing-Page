48a274d4053fe67833807ab193f75936696ff927
Kunal <kunalhoonmai@gmail.com>
2026-09-08 12:21:40 +0530

    configure MUI theme and providers

---
+++ src/components/providers/ThemeProvider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
+
+import { theme } from "@/theme/theme";
+
+export default function ThemeProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <AppRouterCacheProvider>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </MuiThemeProvider>
+    </AppRouterCacheProvider>
+  );
+}
